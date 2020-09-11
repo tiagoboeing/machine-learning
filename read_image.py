97f961debc4db50c2bba8f23c393015c25db555b
Bruno <bruno@dump.net.br>
2020-09-10 23:29:59 -0300

    features adjustments
    all features was returning 0.0

--- read_image.py
+++ read_image.py
@@ -1,118 +1,143 @@
 import os
+import sys
 import cv2
 from logger import Logger
 from range_color import Range
-class ReadImage():  
+
+
+class ReadImage():
     def __init__(self):
-      self.__width = 0
-      self.__height = 0
-      self.__bartOrangeShirt = 0
-      self.__bartBlueShorts = 0
-      self.__bartBlueShoe = 0
-      self.__homerBluePants = 0
-      self.__homerBrownMouth = 0
-      self.__homerGreyShoe = 0
-      self.__renderedImage = None
-      self.__features = []                  
+        self.__width = 0
+        self.__height = 0
+        self.__bartOrangeShirt = 0
+        self.__bartBlueShorts = 0
+        self.__bartBlueShoe = 0
+        self.__homerBluePants = 0
+        self.__homerBrownMouth = 0
+        self.__homerGreyShoe = 0
+        self.__renderedImage = None
+        self.__features = []
+        self.__cloneImage = False
 
-    def read(self, img, cloneImage = False):
-      Logger.log(f'Image received {img}') 
-      image = cv2.imread(img)       
+    def read(self, img, cloneImage=False):
+        Logger.log(f'Image received {img}')
+        image = cv2.imread(img)
 
-      if cloneImage == True:
-        Logger.log('Cloned image')
-        self.__renderedImage = image.copy()                  
+        self.__cloneImage = cloneImage
+
         self.__height, self.__width, channels = image.shape
 
-      Logger.log('Handle width and height')
-      for height in range(self.__height):
-        for width in range(self.__width):
-          pixel = image[height, width]
-          self.handleRangeColors(pixel, width, height)          
+        if self.__cloneImage == True:
+            Logger.log('Cloned image')
+            self.__renderedImage = image.copy()
 
-      definedFeatures = self.normalizeFeatures(img)
+        Logger.log('Handle width and height')
+        for height in range(self.__height):
+            for width in range(self.__width):
+                pixel = image[height, width]
+                self.handleRangeColors(pixel, width, height)
 
-      if cloneImage == True:
-        cv2.imshow('image', self.__renderedImage)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        definedFeatures = self.normalizeFeatures(img)
 
-      return definedFeatures
+        if self.__cloneImage == True:
+            cv2.imshow('image', self.__renderedImage)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        return definedFeatures
 
     """
       Receive a pixel (R, G, B) and call range_color.py
       https://stackoverflow.com/questions/28981417/how-do-i-access-the-pixels-of-an-image-using-opencv-python/50588950
     """
+
     def handleRangeColors(self, pixel, index_width, index_height):
         range = Range()
         b, g, r = pixel
-        
+
         if range.isBartOrangeShirt(r, g, b):
-          self.__bartOrangeShirt += 1 
-          self.set_color(self.__bartOrangeShirt, index_width, index_height)
-        
+            self.__bartOrangeShirt += 1
+
+            if self.__cloneImage == True:
+                self.set_color(self.__bartOrangeShirt,
+                               index_width, index_height)
+
         if index_width > (self.__height / 2) and range.isBartBlueShorts(r, g, b):
-          self.__bartBlueShorts += 1 
-          self.set_color(self.__bartBlueShorts, index_width, index_height)
+            self.__bartBlueShorts += 1
+
+            if self.__cloneImage == True:
+                self.set_color(self.__bartBlueShorts,
+                               index_width, index_height)
 
         if index_width > (self.__height / 2 + self.__height / 3) and range.isBartShoe(r, g, b):
-          self.__bartBlueShoe += 1 
-          self.set_color(self.__bartBlueShoe, index_width, index_height)
-        
-        if range.isHomerBluePants(r, g, b):
-          self.__homerBluePants += 1 
-          self.set_color(self.__homerBluePants, index_width, index_height)
-        
-        if index_width < (self.__height / 2 + self.__height / 3) and range.isHomerMouth(r, g, b):
-          self.__homerBrownMouth += 1 
-          self.set_color(self.__homerBrownMouth, index_width, index_height)
-        
-        if index_width > (self.__height / 2 + self.__height / 3) and range.isHomerShoe(r, g, b):
-          self.__homerGreyShoe += 1 
-          self.set_color(self.__homerGreyShoe, index_width, index_height)
+            self.__bartBlueShoe += 1
 
+            if self.__cloneImage == True:
+                self.set_color(self.__bartBlueShoe, index_width, index_height)
+
+        if range.isHomerBluePants(r, g, b):
+            self.__homerBluePants += 1
+
+            if self.__cloneImage == True:
+                self.set_color(self.__homerBluePants,
+                               index_width, index_height)
+
+        if index_width < (self.__height / 2 + self.__height / 3) and range.isHomerMouth(r, g, b):
+            self.__homerBrownMouth += 1
+
+            if self.__cloneImage == True:
+                self.set_color(self.__homerBrownMouth,
+                               index_width, index_height)
+
+        if index_width > (self.__height / 2 + self.__height / 3) and range.isHomerShoe(r, g, b):
+            self.__homerGreyShoe += 1
+
+            if self.__cloneImage == True:
+                self.set_color(self.__homerGreyShoe, index_width, index_height)
 
     """
       TODO: estudar como associar uma variável recebida com o self
       Ex.: receber `variable` e associar self.variable += 1
     """
+
     def set_color(self, variable, index_width, index_height):
-      self.__renderedImage[index_height][index_width] = [0, 255, 128]
+        self.__renderedImage[index_height][index_width] = [0, 255, 128]
 
     def calcNormalize(self, value):
-      if(value != 0.0):
-        return (value / (self.__width * self.__height)) * 100
+        if(value != 0.0):
+            return (value / (self.__width * self.__height)) * 100
 
-      return 0.0
+        return 0.0
 
     """
       Normalizes the features by the number of total pixels of the image to % 
     """
-    def normalizeFeatures(self, img):      
-      Logger.log('Normalize Features')      
-      
-      self.__bartOrangeShirt = self.calcNormalize(self.__bartOrangeShirt);
-      self.__bartBlueShorts = self.calcNormalize(self.__bartBlueShorts);
-      self.__bartBlueShoe = self.calcNormalize(self.__bartBlueShoe);
-      self.__homerBluePants = self.calcNormalize(self.__homerBluePants);
-      self.__homerBrownMouth = self.calcNormalize(self.__homerBrownMouth);
-      self.__homerGreyShoe = self.calcNormalize(self.__homerGreyShoe);        
-    
-      bartOrHome = 0.0 # Bart
-      filename = os.path.basename(img)[0]
-          
-      if filename == 'h':
-        bartOrHome = 1.0 # Homer
 
-      features = [
-        self.__bartOrangeShirt, 
-        self.__bartBlueShorts, 
-        self.__bartBlueShoe, 
-        self.__homerBluePants, 
-        self.__homerBrownMouth,
-        self.__homerGreyShoe,
-        bartOrHome
-      ]
+    def normalizeFeatures(self, img):
+        Logger.log('Normalize Features')
 
-      Logger.log(features)
-      return features
+        self.__bartOrangeShirt = self.calcNormalize(self.__bartOrangeShirt)
+        self.__bartBlueShorts = self.calcNormalize(self.__bartBlueShorts)
+        self.__bartBlueShoe = self.calcNormalize(self.__bartBlueShoe)
+        self.__homerBluePants = self.calcNormalize(self.__homerBluePants)
+        self.__homerBrownMouth = self.calcNormalize(self.__homerBrownMouth)
+        self.__homerGreyShoe = self.calcNormalize(self.__homerGreyShoe)
+
+        bartOrHome = 0.0  # Bart
+        filename = os.path.basename(img)[0]
+
+        if filename == 'h':
+            bartOrHome = 1.0  # Homer
+
+        features = [
+            self.__bartOrangeShirt,
+            self.__bartBlueShorts,
+            self.__bartBlueShoe,
+            self.__homerBluePants,
+            self.__homerBrownMouth,
+            self.__homerGreyShoe,
+            bartOrHome
+        ]
+
+        Logger.log(features)
+        return features
--- weka.py
+++ weka.py
@@ -3,10 +3,11 @@ from os.path import isfile, join
 from read_image import ReadImage
 from logger import Logger
 
+
 class Weka():
-  def __init__(self, images_directory):
-    self.images_directory = images_directory
-    self.header = '''
+    def __init__(self, images_directory):
+        self.images_directory = images_directory
+        self.header = '''
     @relation caracteristicas\n\n
     @attribute laranja_camisa_bart real\n
     @attribute azul_calcao_bart real\n
@@ -17,18 +18,21 @@ class Weka():
     @attribute classe {Bart, Homer}\n\n
     @data\n'''
 
-  def list_directory_files(self):
-    Logger.log('Reading all folder files')
-    onlyfiles = [f for f in listdir(self.images_directory) if isfile(join(self.images_directory, f))]
-    Logger.log(f'\n{len(onlyfiles)} images found in {self.images_directory} directory!', True)
-    return onlyfiles
+    def list_directory_files(self):
+        Logger.log('Reading all folder files')
+        onlyfiles = [f for f in listdir(self.images_directory) if isfile(
+            join(self.images_directory, f))]
+        Logger.log(
+            f'\n{len(onlyfiles)} images found in {self.images_directory} directory!', True)
+        return onlyfiles[0:3]
 
-  def extract(self):
-    imagesData = []
-    for index, image in enumerate(list(self.list_directory_files())):
-      Logger.log(f'Extracting characteristics from {image}')
-      imagesData.append(ReadImage().read(f'{self.images_directory}/{image}'))
-      Logger.log(f'Data added to index {index}', True)
+    def extract(self):
+        imagesData = []
+        for index, image in enumerate(list(self.list_directory_files())):
+            Logger.log(f'Extracting characteristics from {image}')
+            imagesData.append(ReadImage().read(
+                f'{self.images_directory}/{image}'))
+            Logger.log(f'Data added to index {index}', True)
 
-    # TODO: paramos aqui
-    Logger.log(imagesData)
+        # TODO: paramos aqui
+        Logger.log(imagesData)
