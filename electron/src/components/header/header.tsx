74548ce7e61e5364f1d6b3fadf0b34497936cc8d
Phillipe Vieira <phillipe.vieira@digitalpages.com.br>
2020-09-24 20:31:09 -0300

    Include electron - front project

---
+++ electron/src/components/header/header.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { HeaderWrapper, NavButton } from "./style";
+
+export interface IHeaderProps {}
+
+export interface IHeaderState {}
+
+export default class Header extends React.Component<
+  IHeaderProps,
+  IHeaderState
+> {
+  private ipcRenderer?: any;
+
+  constructor(props: IHeaderProps) {
+    super(props);
+    this.state = {};
+  }
+
+    componentDidMount() {
+    if (!this.ipcRenderer) {
+      this.initializeIpcRenderer();
+    }
+  }
+
+  initializeIpcRenderer = () => {
+    if (!window || !window.process || !window.require) {
+      throw new Error(`Unable to require renderer process`);
+    }
+    this.ipcRenderer = window.require("electron").ipcRenderer;
+  };
+
+  openTrainingMode = () => {
+    this.ipcRenderer.send("open-training", "ping");
+  };
+
+  exitApp = () => {
+    this.ipcRenderer.send("close-program", "ping");
+  };
+
+  public render() {
+    return (
+      <HeaderWrapper>
+        <NavButton onClick={() => this.openTrainingMode()}>Treinamento</NavButton>
+        <NavButton onClick={() => this.exitApp()}>Sair</NavButton>
+      </HeaderWrapper>
+    );
+  }
+}
