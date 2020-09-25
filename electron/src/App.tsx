ed41216631a3132ad0f2cd07fddc2c10fe2df296
Phillipe Vieira <phillipe.vieira@digitalpages.com.br>
2020-09-24 22:02:07 -0300

    fix content

--- electron/src/App.tsx
+++ electron/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './components/header/header';
-import Feature from './components/Feature';
 import Content from './components/content/content';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Feature />
+      <Header />      
       <Content />
     </div>
   );
