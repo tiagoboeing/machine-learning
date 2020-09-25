d5753c19f14ab494d1ade0b3839b50e0938afaf0
Diou Medeiros <diou.182@gmail.com>
2020-09-24 22:52:14 -0300

    feat: Created Features component example

--- electron/src/components/Feature/index.tsx
+++ electron/src/components/Feature/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
-import { Container } from './styles';
+import * as S from './styles';
 
 const Feature: React.FC = () => {
   return (
-    <Container>
-      <p>Teste</p>
-      <p>Teste</p>
-      <p>Teste</p>
-      <p>Teste</p>
-    </Container>
+    <S.Container>
+      <S.Content>
+        <header>Caracteristicas</header>
+        <S.Wrapper>
+          <div>
+            <S.Subtitle>Bart</S.Subtitle>
+            <S.List>
+              <S.ListItem>Cu</S.ListItem>
+              <S.ListItem>Zao</S.ListItem>
+              <S.ListItem>Né?</S.ListItem>
+            </S.List>
+          </div>
+          <div>
+            <S.Subtitle>Homer</S.Subtitle>
+            <S.List>
+              <S.ListItem>De</S.ListItem>
+              <S.ListItem>Li</S.ListItem>
+              <S.ListItem>Cia</S.ListItem>
+            </S.List>
+          </div>
+        </S.Wrapper>
+      </S.Content>
+    </S.Container>
   );
 };
 
--- electron/src/components/content/content.tsx
+++ electron/src/components/content/content.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
+import Feature from '../Feature';
+
 import { ContentWrapper, LeftContent, RightContent } from './style';
 
-
-export interface IContentProps {
-}
+export interface IContentProps {}
 
 export default class Content extends React.Component<IContentProps> {
-
-    public render() {
-        return (
-            <ContentWrapper>
-                <LeftContent></LeftContent>
-                <RightContent></RightContent>
-            </ContentWrapper>
-        );
-    }
+  public render() {
+    return (
+      <ContentWrapper>
+        <LeftContent></LeftContent>
+        <RightContent>
+          <Feature />
+        </RightContent>
+      </ContentWrapper>
+    );
+  }
 }
