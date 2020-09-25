54be6d8ecdb740665a9493dad9150b92fcf0b889
Diou Medeiros <diou.182@gmail.com>
2020-09-24 21:50:37 -0300

    feat: Initialized Features component

--- electron/src/App.tsx
+++ electron/src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import Header from "./components/header/header";
-import "./App.css";
+import React from 'react';
+import Header from './components/header/header';
+import Feature from './components/Feature';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Feature />
     </div>
   );
-
-
 }
 
 export default App;
---
+++ electron/src/components/Feature/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Feature: React.FC = () => {
+  return (
+    <Container>
+      <p>Teste</p>
+      <p>Teste</p>
+      <p>Teste</p>
+      <p>Teste</p>
+    </Container>
+  );
+};
+
+export default Feature;
