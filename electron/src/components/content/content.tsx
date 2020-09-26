b33678c56dc3f8f825bb2d1c3493dfe138158a28
Bruno <bruno@dump.net.br>
2020-09-25 23:20:25 -0300

    comunicação entre python e electron

--- electron/src/components/content/content.tsx
+++ electron/src/components/content/content.tsx
@@ -1,64 +1,84 @@
-import * as React from 'react';
-import Feature from '../feature';
-import ImageSelector from '../image-selector/image-selector';
-import { ContentWrapper, LeftContent, RightContent, Btn, DisableBtn } from './style';
+import * as React from "react";
+import Feature from "../Feature";
+import ImageSelector from "../image-selector/image-selector";
 
-export interface IContentProps { }
+import {
+  ContentWrapper,
+  LeftContent,
+  RightContent,
+  Btn,
+  DisableBtn,
+} from "./style";
+
+export interface IContentProps {}
 
 export interface IContentState {
-    ipcRenderer: any
-    image?: object
+  ipcRenderer: any;
+  image?: { path?: string; preview?: any };
 }
 
-export default class Content extends React.Component<IContentProps, IContentState> {
-    constructor(props: IContentProps) {
-        super(props);
-        this.state = {
-            ipcRenderer: null,
-        };
-    }
-    componentDidMount() {
-        if (!this.state.ipcRenderer) {
-            this.initializeIpcRenderer();
-        }
-    }
-
-    initializeIpcRenderer = () => {
-        if (!window || !window.process || !window.require) {
-            throw new Error(`Unable to require renderer process`);
-        }
-        this.setState({ ipcRenderer: window.require('electron').ipcRenderer });
+export default class Content extends React.Component<
+  IContentProps,
+  IContentState
+> {
+  constructor(props: IContentProps) {
+    super(props);
+    this.state = {
+      ipcRenderer: null,
     };
-
-    handleImage = (image: object) => {
-        console.log("SELECTED-IMAGE", image);
-        this.setState({ image: image });
-
+  }
+  componentDidMount() {
+    if (!this.state.ipcRenderer) {
+      this.initializeIpcRenderer();
     }
+  }
 
-    classifyAction = () => {
-        const { ipcRenderer, image } = this.state;
-        if (typeof image == 'object') {
-            ipcRenderer.send('classify-image', JSON.stringify({ path: image.path, preview: image.preview }));
-        }
+  initializeIpcRenderer = () => {
+    if (!window || !window.process || !window.require) {
+      throw new Error(`Unable to require renderer process`);
     }
+    this.setState({ ipcRenderer: window.require("electron").ipcRenderer });
+  };
 
-    public render() {
-        const { image } = this.state;
-        return (
-            <ContentWrapper>
-                <LeftContent>
-                    <ImageSelector changeImage={this.handleImage} />
-                    {image ?
-                        <Btn onClick={() => this.classifyAction()} >Classificar</Btn>
-                        :
-                        <DisableBtn >Classificar</DisableBtn>
-                    }
-                </LeftContent>
-                <RightContent>
-                    <Feature />
-                </RightContent>
-            </ContentWrapper>
-        );
-    }
+  handleImage = (image: object) => {
+    console.log("SELECTED-IMAGE", image);
+    this.setState({ image: image });
+  };
+
+  sendToPython = () => {
+    const { ipcRenderer } = this.state;
+
+    ipcRenderer.send("open-training");
+  };
+
+  classifyAction = () => {
+    // const { ipcRenderer, image } = this.state;
+    // if (typeof image == "object") {
+    //   ipcRenderer.send(
+    //     "classify-image",
+    //     JSON.stringify({ path: image.path, preview: image.preview })
+    //   );
+    // }
+
+    this.sendToPython();
+  };
+
+  public render() {
+    const { image } = this.state;
+    return (
+      <ContentWrapper>
+        <LeftContent>
+          <ImageSelector changeImage={this.handleImage} />
+          {!image ? (
+            <Btn onClick={() => this.classifyAction()}>Classificar</Btn>
+          ) : (
+            <DisableBtn>Classificar</DisableBtn>
+          )}
+        </LeftContent>
+        <RightContent>
+          <Feature />
+        </RightContent>
+      </ContentWrapper>
+    );
+  }
 }
