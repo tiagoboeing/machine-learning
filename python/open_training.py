db12bb57d776d05c0c94a875c387d89ee6e4374b
Tiago Boeing <tiago.boeing@senior.com.br>
2020-09-26 21:02:35 -0300

    Remove main

--- python/open_training.py
+++ python/open_training.py
@@ -1,12 +1,9 @@
 import os
-import sys
-import json
 
 from weka import Weka
 from classifier import Classifier
 
 if __name__ == "__main__":
-
     features = Weka(os.path.dirname(__file__) +
                     '/images').extractTo(fileName='caracteristicas')
 
